a9a8b58cea7a605f8d16b1a3161fa63e4f510f09
Hiram J. Pérez <hi@worg.xyz>
2017-09-10 13:58:21 -0500

    feat: basic search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import debounce from 'lodash.debounce'
 import * as BooksAPI from './BooksAPI'
 import BookList from './BookList'
 import SearchForm from './SearchForm'
@@ -10,6 +11,7 @@ class BooksApp extends React.Component {
     super(props);
     this.state = {
       books: [],
+      filtered: [],
       searchQuery: '',
     };
   }
@@ -37,13 +39,39 @@ class BooksApp extends React.Component {
     });
   }
 
+  handleSearch = (e) => {
+    const { target: { value } } = e;
+    console.warn('E:', value);
+    this.performSearch(value);
+  }
+
+  performSearch = debounce(searchQuery => {
+    this.setState({ searchQuery }, () => {
+      BooksAPI.search(searchQuery, 10).then(result => {
+        console.warn('filtered', result);
+        let filtered = result;
+        if (result.error) {
+          filtered = result.items;
+        }
+
+        this.setState({ filtered });
+      });
+    });
+  }, 200, { leading: false, trailing: true })
+
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
           <BookList books={this.state.books} updateShelf={this.updateShelf} />
         )}/>
-        <Route path='/search/:query' render={SearchForm}/>
+        <Route path='/search/:query?' render={({ match }) => (
+          <SearchForm
+            books={this.state.filtered}
+            query={match.params.query}
+            updateShelf={this.updateShelf}
+            handleSearch={this.handleSearch} />
+        )}/>
       </div>
     )
   }
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import BookItem from './BookItem';
 
-const SearchForm = () => (
+const SearchForm = ({ query, books, handleSearch, updateShelf }) => (
   <div className="search-books">
     <div className="search-books-bar">
       <Link className='close-search' to='/'>Close</Link>
@@ -14,13 +16,33 @@ const SearchForm = () => (
           However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
           you don't find a specific author or title. Every search is limited by search terms.
         */}
-        <input type="text" placeholder="Search by title or author"/>
+        <input
+          type="text"
+          placeholder="Search by title or author"
+          defaultValue={query}
+          onChange={handleSearch} />
       </div>
     </div>
     <div className="search-books-results">
-      <ol className="books-grid"></ol>
+      <ol className="books-grid">
+        {books.map((b, i) => (
+          <li key={i}>
+            <BookItem book={b} onChange={updateShelf} />
+          </li>
+        ))}
+      </ol>
     </div>
   </div>
 );
+
+SearchForm.propTypes = {
+  query: PropTypes.string,
+  updateShelf: PropTypes.func.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+};
+
+SearchForm.defaultProps = {
+  query: '',
+};
 
 export default SearchForm;
